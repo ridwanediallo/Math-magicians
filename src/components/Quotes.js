09722f9503c236a653b6bbfd0033c818355a342a
ridwan <ridwanediallo99@gmail.com>
2022-04-23 07:30:29 +0000

    Add content in home and quote page

---
+++ src/components/Quotes.js
@@ -0,0 +1,12 @@
+const Quote = () => {
+  return (
+    <div>
+      <h1>
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding. -William Paul Thurston
+      </h1>
+    </div>
+  );
+};
+
+export default Quote;
