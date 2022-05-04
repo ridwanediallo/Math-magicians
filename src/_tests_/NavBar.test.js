a35b88c4b4d2119db63ef7f4515e3fc25982af03
ridwan <ridwanediallo99@gmail.com>
2022-05-04 21:46:38 +0000

    Add tests

--- src/_tests_/NavBar.test.js
+++ src/_tests_/NavBar.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+
 test('should render NavBar', () => {
   render(
     <nav className="nav">
